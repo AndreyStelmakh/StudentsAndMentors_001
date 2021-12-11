0fcf1dbb03a6eb291e69fe9a08ca6eff27a20297
Andrey Stelmakhgit config --global user.email andrey.stelmakh@live.rugit config --global user.namegit config --global user.name Andrey <andrey.stelmakh@live.ru>
2021-12-11 14:55:20 +0300

    Незначительные правки

--- StudentsAndMentors.py
+++ StudentsAndMentors.py
@@ -50,11 +50,13 @@ class Student:
         else:
             return 'Ошибка'
 
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
+
 
 class Lecturer(Mentor):
     def __init__(self, name, surname):
@@ -85,6 +87,7 @@ class Lecturer(Mentor):
                 values += [value]
         return sum(values) / (len(values) if values else 1)
 
+
 class Reviewer(Mentor):
     def __str__(self):
         return f"Имя: {self.name} \nФамилия: {self.surname}"
@@ -98,6 +101,7 @@ class Reviewer(Mentor):
         else:
             return 'Ошибка'
 
+
 def average_hw_rate_by_course(students, course):
     sum = 0
     count = 0
@@ -108,6 +112,7 @@ def average_hw_rate_by_course(students, course):
     result = sum / (count if count > 0 else 1)
     print(f"Средняя оценка за домашние задания по {course}:", result)
 
+
 def average_lecturer_rate_by_course(lecturers, course):
     sum = 0
     count = 0
@@ -117,6 +122,7 @@ def average_lecturer_rate_by_course(lecturers, course):
             count += 1
     result = sum / (count if count > 0 else 1)
     print(f"Средняя оценка лекторов по курсу {course}:", result)
+
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress.append('Python')
@@ -138,7 +144,6 @@ print(best_student)
 print(best_student.grades)
 print(best_student.courses_in_progress)
 
-print("Сравнение студентов: ", best_student < best_student_2)
 
 cool_lecturer = Lecturer('Some', 'Buddy')
 cool_lecturer.courses_attached += ['Python']
@@ -150,7 +155,7 @@ cool_lecturer3 = Lecturer('Sone', 'Bunny')
 cool_lecturer3.courses_attached += ['Python']
 cool_lecturer3.courses_attached += ['C++']
 
-best_student.rate_lecturer( cool_lecturer, 'Python', 10)
+best_student.rate_lecturer(cool_lecturer, 'Python', 10)
 
 best_student.rate_lecturer(cool_lecturer3, 'Python', 10)
 best_student.rate_lecturer(cool_lecturer3, 'Python', 9.5)
@@ -159,6 +164,9 @@ best_student_2.rate_lecturer(cool_lecturer, 'Python', 10)
 best_student_2.rate_lecturer(cool_lecturer2, 'Python', 9.5)
 
 print(cool_lecturer)
+
+print("Сравнение студентов: ", best_student < best_student_2)
+print("Сравнение лекторов", cool_lecturer < cool_lecturer3)
 
 average_hw_rate_by_course([best_student, best_student_2], 'Python')
 average_lecturer_rate_by_course([cool_lecturer, cool_lecturer2, cool_lecturer3], 'Python')
